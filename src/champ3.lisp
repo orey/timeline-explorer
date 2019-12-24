a310d75f6e25c49c0d3787f328b56713050f8ba3
Olivier Rey <rey.olivier@gmail.com>
2019-12-24 11:14:01 +0100

    stuff

--- src/champ3.lisp
+++ src/champ3.lisp
@@ -6,11 +6,35 @@
 (defparameter DIR-WC  "*/" "Wild card default for directories. Could be implementation specific")
 (defparameter FILE-WC "*" "Wild card default for files.")
 
-(defvar sampledir "/home/olivier/olivier2/Bibliotheque/IT/Lisp/")
+(defparameter LAST-DIR-CHAR "/")
 
-(defun list-files (dir)
-  (let ((pn (pathname dir)))
-    (directory (concatenate 'string pn FILE-WC)))
+;(defun list-files (dir)
+; (directory (concatenate
+
+(defun last-char (str)
+  "Get the last char"
+  (subseq str (- (length str) 1)))
+
+(defun folder-p (str)
+  "Check the last char of the str passed and return true if it is /"
+  (if (string-equal (last-char str) LAST-DIR-CHAR) T NIL))
+
+(defun filter-files (lst)
+  "lst is a list of pathnames and not string, typically our from directory"
+  (remove-if 'folder-p (mapcar 'namestring lst)))
+
+(defun filter-folders (lst)
+  "lst is a list of pathnames and not string, typically our from directory"
+  (remove-if-not 'folder-p (mapcar 'namestring lst)))
+
+(defun filter (lst)
+  "lst is a list of pathnames and not string, typically our from directory"
+  (values (remove-if-not 'folder-p (mapcar 'namestring lst))
+          (remove-if     'folder-p (mapcar 'namestring lst))))
+
+(defun explore-and-collect (dir acc)
+  (if (null acc)
+      (multiple-value-bind (filter (directory dir))
 
  
 
