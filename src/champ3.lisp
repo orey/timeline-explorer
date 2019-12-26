7c1614a657a9aa5a316768eae7195eabba10abcb
Olivier Rey <rey.olivier@gmail.com>
2019-12-26 17:19:23 +0100

    stable version

--- src/champ3.lisp
+++ src/champ3.lisp
@@ -6,23 +6,28 @@
 (defparameter LAST-DIR-CHAR "/")
 (defparameter PATTERN       "*.*")
 
+
 (defun last-char (str)
   "Get the last char of a string."
   (subseq str (- (length str) 1)))
+
 
 (defun folder-p (str)
   "Check the last char of the str passed and return true if it is '/'.
    Used for folders as namestrings."
   (if (string-equal (last-char str) LAST-DIR-CHAR) T NIL))
 
+
 (defun check-folder (dir)
   "Check if it is a real folder"
   (if (not (probe-file dir)) NIL
     (folder-p (namestring (car (directory dir))))))
 
+
 (defun files-in-dir (dir)
   "dir is a string."
   (remove-if 'folder-p (mapcar 'namestring (directory (concatenate 'string dir PATTERN)))))
+
 
 (defun dirs-in-dir (dir)
   "dir is a string"
@@ -39,6 +44,7 @@
                             acc1))
               (setf acc (find-files1 acc dir))))))
 
+
 (defun get-file-seconds (f)
   (with-open-file (s f)
                   (file-write-date s)))
@@ -48,7 +54,8 @@
     (dolist (f acc)
       (setf out (concatenate 'list out (list (cons (get-file-seconds f) (list f))))))
     out))
-    
+
+
 (defun list> (a b)
   (if (> (car a) (car b)) T nil))
 
@@ -56,8 +63,6 @@
 (defun format-elem (elem strea)
   (let ((dt (multiple-value-list (decode-universal-time (car elem))))
         (pn (car (directory (cadr elem)))))
-    ;(format strea "<p>~d-~d-~d, ~d:~d:~d | <a href=\"~A\" target=\"_new\">~A</a></p>"
-;    (format strea "<p>~d-~d-~d, ~2,'0d:~2,'0d:~2,'0d | <a href=\"~A\" target=\"_new\">~A</a></p>"
     (format strea "<p>~2,'0d-~2,'0d-~2,'0d - ~2,'0d:~2,'0d:~2,'0d | <a href=\"~A\" target=\"_new\">~A</a></p>"
             (sixth dt)
             (fifth dt)
@@ -67,6 +72,7 @@
             (first dt)
             (cadr elem)
             (concatenate 'string (pathname-name pn) "." (pathname-type pn)))))
+
 
 (defun format-date (d level strea sidebar)
   (cond ((eql level 1) ; change of year
@@ -120,17 +126,14 @@
 <body> \
 <div class=\"content\">"))
 
+
 (defun format-sidebar (strea)
-  (format strea
-          "</div> \
-<div class=\"sidenav\">"))
+  (format strea "</div><div class=\"sidenav\">"))
 
 
 (defun format-footer (strea)
-  (format strea
-          "</div> \
-</body> \
-</html>"))
+  (format strea "</div></body></html>"))
+
 
 (defun generate-page (dir)
   (let ((sorted-list (sort (build-map (find-files dir)) #'list>))
@@ -163,12 +166,12 @@
   (build-map (find-files "/home/olivier/Documents/github/")))
 
 (defun test5 ()
-  (generate-page "/home/olivier/Documents/OREYBOX/Biblio/"))
-
-(defun test6 ()
   (format-header T)
   (print "------------------")
   (format-footer T))
+
+(defun test6 ()
+  (generate-page "/home/olivier/Documents/OREYBOX/Biblio/"))
 
 (defun test-suite ()
   (test1)
